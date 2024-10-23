b155bdeaca94f9c1ed63a554a2105fb6a71137a8
bibijeebi <bennyforeman1@gmail.com>
2024-10-23 17:05:59 -0400

    refactor: migrate from legacy nix to flake-only setup

--- flake.nix
+++ flake.nix
@@ -1,21 +1,82 @@
 {
-  description = "nx: a command line wrapper tool for nix built in rust";
+  description = "A friendly wrapper for common Nix commands";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
+
   outputs = {
     self,
     nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgsFor = nixpkgs.legacyPackages;
-  in {
-    packages = forAllSystems (system: {
-      default = pkgsFor.${system}.callPackage ./. {};
-    });
-    devShells = forAllSystems (system: {
-      default = pkgsFor.${system}.callPackage ./shell.nix {};
-    });
-  };
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          rust-bin.stable.latest.default
+        ];
+
+        buildInputs = with pkgs;
+          [
+            wayland
+            wayland-protocols
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            vulkan-loader
+            pkg-config
+          ]
+          ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.AppKit
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+          ];
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
+          src = ./.;
+
+          inherit nativeBuildInputs buildInputs;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              # Add any git dependencies here if needed
+            };
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          inherit nativeBuildInputs buildInputs;
+
+          packages = with pkgs; [
+            rust-analyzer
+            clippy
+            rustfmt
+          ];
+
+          RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
+        };
+      }
+    );
 }
