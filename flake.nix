1b4ec5087a241de301b0dc8be01189d4c6cb5860
bibijeebi <bennyforeman1@gmail.com>
2024-10-23 16:07:35 -0400

    feat: add flake.nix and restructure Nix configuration files

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "nx: a command line wrapper tool for nix built in rust";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    packages = forAllSystems (system: {
+      default = pkgsFor.${system}.callPackage ./. {};
+    });
+  };
+}
--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [rustc cargo gcc rustfmt clippy rust-analyzer];
-
-  # Certain Rust tools won't work without this
-  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
-  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  # Get dependencies from the main package
+  inputsFrom = [(pkgs.callPackage ./default.nix {})];
+  # Additional tooling
+  buildInputs = with pkgs; [
+    rust-analyzer # LSP Server
+    rustfmt # Formatter
+    clippy # Linter
+  ];
 }
